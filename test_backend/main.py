f714fdb7b83f633b80c889e4da18350b44227f70
Mikhail Trifonov <m.trifonov@innopolis.university>
2024-07-07 20:30:38 +0300

    Remove comments

--- test_backend/main.py
+++ test_backend/main.py
@@ -20,11 +20,6 @@ class TestSaveLLMResponseAsNote(TestCase):
 
     @mock.patch("backend.apps.ollama.main.save_llm_response_as_note")
     def test_save_llm_response_as_note_saves_in_upload_dir(self, mock_save_llm_response_as_note):
-        # Setup
         llm_response_text = "Text of LLM response for unit testing"
-        
-        # Act
         path = asyncio.run(save_llm_response_as_note(llm_response_text))
-
-        # Assert
         self.assertTrue(os.path.exists(path))
