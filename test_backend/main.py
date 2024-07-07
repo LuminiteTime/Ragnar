2cd004d81803519f2fcbfd728d5d5bcb6b711924
Mikhail Trifonov <m.trifonov@innopolis.university>
2024-07-07 15:40:37 +0300

    Add unit test for note saving path

--- test_backend/main.py
+++ test_backend/main.py
@@ -1,18 +1,20 @@
 import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
 from unittest import mock, TestCase
 from backend.config import UPLOAD_DIR
 from backend.apps.ollama.main import save_llm_response_as_note
-
+import asyncio
 
 class TestSaveLLMResponseAsNote(TestCase):
     @mock.patch("backend.apps.ollama.main.save_llm_response_as_note")
-    async def test_save_llm_response_as_note_saves_in_upload_dir(self, mock_save_llm_response_as_note):
+    def test_save_llm_response_as_note_saves_in_upload_dir(self, mock_save_llm_response_as_note):
         # Setup
         llm_response_text = "Text of LLM response for unit testing"
-        expected_file_path = os.path.join(UPLOAD_DIR, "Text_of_LLM_response_for.md")
         
         # Act
-        await save_llm_response_as_note(llm_response_text)
+        path = asyncio.run(save_llm_response_as_note(llm_response_text))
 
         # Assert
-        self.assertEquals(os.path.exists(expected_file_path), True)
+        self.assertTrue(os.path.exists(path))
